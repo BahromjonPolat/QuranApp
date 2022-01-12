75476c7cd03bf68bbdb093c05a515618ff582cb2
BahromjonPolat <itjunior.uz@gmail.com>
2022-01-12 21:34:54 +0500

    Size Config

---
+++ lib/core/components/exporting_packages.dart
@@ -0,0 +1,11 @@
+
+// Screens
+export 'package:quran/screens/home/home_page.dart';
+
+// Widgets
+
+// Components
+export 'package:quran/core/components/size_config.dart';
+
+// Constants
+export 'package:quran/core/constants/colors.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+
       ),
       home: const HomePage(),
     );
