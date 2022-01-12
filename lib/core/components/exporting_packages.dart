5bb8cdaf50424021d1c015dd0d63df51887496a7
BahromjonPolat <itjunior.uz@gmail.com>
2022-01-12 21:50:09 +0500

    Kerakli paketlar o'rnatildi

--- lib/core/components/exporting_packages.dart
+++ lib/core/components/exporting_packages.dart
@@ -9,3 +9,10 @@ export 'package:quran/core/components/size_config.dart';
 
 // Constants
 export 'package:quran/core/constants/colors.dart';
+
+// Installed packages
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:provider/provider.dart';
+export 'package:fluttertoast/fluttertoast.dart';
+
