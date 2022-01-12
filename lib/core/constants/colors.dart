bcde2e312b9a96726576540a6b9fd62d7eda22e9
BahromjonPolat <itjunior.uz@gmail.com>
2022-01-12 21:26:30 +0500

    Const colors were written

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class ConstColors {
+  static const Color primary = Color(0xFF672CBC);
+  static const Color grey = Color(0xFF8789A3);
+  static const Color lightGrey = Color(0xFFABAFD7);
+  static const Color dark = Color(0xFF240F4F);
+  static const Color withOpacity = Color(0x0d863ed5);
+  static const Color darkBackground = Color(0xFF091945);
+  static const Color orange = Color(0xFFF9B091);
+
+}
