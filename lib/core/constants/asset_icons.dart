71d74f27a71ef2cd99af213fb3ef219393f6c3fa
BahromjonPolat <itjunior.uz@gmail.com>
2022-01-13 14:31:58 +0500

    Ikonlar qo'shildi

---
+++ lib/core/constants/asset_icons.dart
@@ -0,0 +1,12 @@
+class AssetIcons {
+  static const String book = 'assets/icons/book.svg';
+  static const String bookmark = 'assets/icons/bookmark.svg';
+  static const String dua = 'assets/icons/dua.svg';
+  static const String lamp = 'assets/icons/lamp.svg';
+  static const String hexagon = 'assets/icons/hexagon.svg';
+  static const String play = 'assets/icons/play.svg';
+  static const String prayer = 'assets/icons/prayer.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String share = 'assets/icons/share.svg';
+  static const String sort = 'assets/icons/sort.svg';
+}
